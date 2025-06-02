f45018c8f4f44be040c02a5068b74bee494ff2d0
103_NI'MAH FAUZIYYAH ATOK <nmhfauziyyah@gmail.com>
2025-06-02 18:24:13 +0700

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,22 +1,48 @@
 #include "shell.h"
 #include "kernel.h"
 
+int textColor = 0x07;
+
 int main() {
-  clearScreen();
-  shell();
+    clearScreen(textColor);  
+    shell();
+    return 0;
 }
 
-void printString(char *str)
-{
-  //TODO: Implementasi fungsi untuk mencetak string
+void printString(char *str) {
+    while (*str != '\0') {
+        interrupt(0x10, 0x0E00 + *str, 0, 0, 0);        
+        str++;
+    }
 }
 
-void readString(char *buf)
-{
-  //TODO: Implementasi fungsi untuk membaca string
+void readString(char *buf) {
+    char ch;
+    int i = 0;
+    while (1) {
+        ch = interrupt(0x16, 0x0000, 0, 0, 0) & 0xFF;
+        if (ch == 0x0D) break;
+        if (ch == 0x08) {
+            if (i > 0) {
+                i--;
+                printString("\b \b");
+            }
+        } else {
+            buf[i++] = ch;
+            interrupt(0x10, 0x0E00 + ch, 0, 0, 0);
+        }
+    }
+    buf[i++] = 0x0D;
+    buf[i++] = 0x0A;
+    buf[i] = '\0';  
+    printString("\r\n");
 }
 
-void clearScreen()
-{
-  //TODO: Implementasi fungsi untuk membersihkan layar
+void clearScreen(int color) {
+    int i;
+    for (i = 0; i < 80 * 25 * 2; i += 2) {
+        putInMemory(0xB800, i, ' ');
+        putInMemory(0xB800, i + 1, color); 
+    }
+    interrupt(0x10, 0x0200, 0, 0, 0);
 }
