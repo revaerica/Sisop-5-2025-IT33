4c08e54680b6493b8cfc25c1b54ef65aebbfe4a5
103_NI'MAH FAUZIYYAH ATOK <nmhfauziyyah@gmail.com>
2025-06-02 18:25:51 +0700

    Update kernel.h

--- include/kernel.h
+++ include/kernel.h
@@ -7,8 +7,10 @@ extern void putInMemory(int segment, int address, char character);
 extern int interrupt(int number, int AX, int BX, int CX, int DX);
 unsigned int getBiosTick();
 
+
 void printString(char* str);
 void readString(char* buf);
-void clearScreen();
+void clearScreen(int color);
+
 
 #endif // __KERNEL_H__
